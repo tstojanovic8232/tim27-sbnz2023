6adfdab6ebb6e049ffd5350376589dbc33f3a871
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2023-06-09 11:12:08 +0200

    Rešen problem sa rutiranjem

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from "./landing-page/landing-page.component";
-import { MangaComponent } from "./manga/manga.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {MangaComponent} from "./manga/manga.component";
 
 
 import {BasicHomePageComponent} from "./anime/pages/basic-home-page/basic-home-page.component";
@@ -17,28 +16,22 @@ const routes: Routes = [
     path: '', component: LandingPageComponent
   },
   {
-
-    path: 'manga', component: MangaComponent
-  },
-  {
-    path: 'anime', component: LandingPageComponent,
-
-    // path: 'home', redirectTo: '', pathMatch: "full"
+    path: 'home', redirectTo: '', pathMatch: "full"
   },
   {
     path: 'manga', component: MangaComponent
   },
   {
-    path: 'signup', component: LoginComponent
-  },
-  {
-    path: 'login', component: LoginComponent
-  },
-  {
-    path: 'anime', component:BasicAnimePagesComponent, children:
+    path: 'anime', component: BasicAnimePagesComponent, children:
       [
         {
           path: '', component: BasicHomePageComponent
+        },
+        {
+        path: 'signup', component: LoginComponent
+        },
+        {
+          path: 'login', component: LoginComponent
         },
         {
           path: 'browse', component: BrowseComponent
